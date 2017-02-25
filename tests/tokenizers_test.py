3fb88f1049634b5788d61e7764a1241e10c99ca1
Verdandi <aimed.fire@gmail.com>
2017-02-25 12:09:08 +0300

    Added new class FunctionDefinitionSearcher and tests for it

--- tests/tokenizers_test.py
+++ tests/tokenizers_test.py
@@ -6,6 +6,7 @@
 import unittest
 
 from tokenizers.function import FunctionDeclarationSearcher
+from tokenizers.function import FunctionDefinitionSearcher
 from tokenizers.function import tokens
 
 class FunctionDeclarationSearcherTest(unittest.TestCase):
@@ -35,6 +36,37 @@ class FunctionDeclarationSearcherTest(unittest.TestCase):
             found_token = tokenizer.findToken(line.strip())
 
             self.assertEquals(tokens.FUNCTION_DECLARATION, found_token)
+            short_name, full_name = tokenizer.found_token
+            self.assertEquals('function', short_name)
+            self.assertEquals(line.strip(), full_name)
+
+class FunctionDefinitionSearcherTest(unittest.TestCase):
+    """Tests for class FunctionDeclarationSearcher"""
+
+    def test_should_find_function_definition(self):
+        """FunctionDeclarationSearcher should find function definition in headers"""
+        tokenizer = FunctionDefinitionSearcher()
+
+
+        with open('tests/function_definition_example2', 'r') as example:
+            for i in range(0,6):
+                line = example.readline()
+                found_token = tokenizer.findToken(line.strip())
+                self.assertEquals(tokens.NOT_ENOUGH_DATA, found_token)
+
+            line = example.readline()
+            found_token = tokenizer.findToken(line.strip())
+            self.assertEquals(tokens.FUNCTION_DEFINITION, found_token)
+            short_name, full_name = tokenizer.found_token
+            self.assertEquals('function134', short_name)
+            self.assertEquals('int function134(int val1, double val2, char* double3, void(*)(char, int, double*, int(*)() ){',
+                              full_name)
+
+        with open('tests/function_definition_example1', 'r') as example:
+            line = example.readline()
+            found_token = tokenizer.findToken(line.strip())
+
+            self.assertEquals(tokens.FUNCTION_DEFINITION, found_token)
             short_name, full_name = tokenizer.found_token
             self.assertEquals('function', short_name)
             self.assertEquals(line.strip(), full_name)
--- tokenizers/function.py
+++ tokenizers/function.py
@@ -10,7 +10,7 @@ from . import tokens
 
 class FunctionDeclarationSearcher:
     def __init__(self):
-        self.__pattern = re.compile(r'^static \w+ (?P<token1>\w+)\(.*\);$|^\w+ (?P<token2>\w+)\(.*\);$')
+        self.__pattern = re.compile(r'^static \w+ (?P<token1>\w+)\(.*\) *;$|^\w+ (?P<token2>\w+)\(.*\) *;$')
         self.__found_token = ''
         self.__token_name = ''
         self.__data_part = ''
@@ -33,6 +33,42 @@ class FunctionDeclarationSearcher:
         else:
             self.__data_part = ''
             return tokens.NOTHING_SPECIAL
+
+    def clear(self):
+        self.__data_part = ''
+
+    @property
+    def found_token(self):
+        return self.__token_name, self.__found_token
+
+class FunctionDefinitionSearcher:
+    def __init__(self):
+        self.__pattern = re.compile(r'^static \w+ (?P<token1>\w+)\(.*\) *{$|^\w+ (?P<token2>\w+)\(.*\) *{$')
+        self.__found_token = ''
+        self.__token_name = ''
+        self.__data_part = ''
+
+    def findToken(self, line):
+        if not line.endswith('{'):
+            if not self.__data_part:
+                self.__data_part = line
+            else:
+                self.__data_part = ' '.join([self.__data_part, line])
+            return tokens.NOT_ENOUGH_DATA
+
+        test_string = self.__data_part + line
+        result = self.__pattern.match(test_string)
+        if result:
+            self.__found_token = test_string
+            self.__token_name = result.group('token1') if result.groupdict()['token1'] else result.group('token2')
+            self.__data_part = ''
+            return tokens.FUNCTION_DEFINITION
+        else:
+            self.__data_part = ''
+            return tokens.NOTHING_SPECIAL
+
+    def clear(self):
+        self.__data_part = ''
 
     @property
     def found_token(self):
