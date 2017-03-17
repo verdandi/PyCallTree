484dd793547557fa3bcc3cbdcd97f99ba035e6d7
Verdandi <aimed.fire@gmail.com>
2017-03-17 15:29:10 +0300

    Small fixes

--- main.py
+++ main.py
@@ -50,44 +50,49 @@ class FileParser:
         if full_path[-2:] != '.h' and full_path[-2:] != '.c':
             return
 
-        for number, line in enumerate(open(full_path, 'r'), start = 1):
-            # we try to skip ends of functions or some expressions
-            if (line.strip() == '}' or (line.startswith('#') and (line.find("ifdef") >= 0 or
-                line.find("endif") >= 0 or line.find("if") >= 0 or line.find("else") >= 0 or
-                line.find("undef") >= 0))):
-                continue
+        try:
+            for number, line in enumerate(open(full_path, 'r'), start = 1):
+                # we try to skip ends of functions or some expressions
+                if (line.strip() == '}' or (line.startswith('#') and (line.find("ifdef") >= 0 or
+                    line.find("endif") >= 0 or line.find("if") >= 0 or line.find("else") >= 0 or
+                    line.find("undef") >= 0))):
+                    continue
 
-            for tokenizer in self.__tokenizers:
-                token = tokenizer.findToken(line.strip())
-                if token == tokens.FUNCTION_DECLARATION:
-                    token_name, detailed_name = tokenizer.found_token
-                    new_token = self.__tokens_map.setdefault(token_name, {'detail': '', 'declarations': [], 'definitions': []})
-                    new_token['detail'] = detailed_name
-                    new_token['declarations'].append(file_name+':'+str(number))
-                    self.clearTokenizers()
-
-                if token == tokens.FUNCTION_DEFINITION:
-                    token_name, detailed_name = tokenizer.found_token
-                    new_token = self.__tokens_map.setdefault(token_name, {'detail': '', 'declarations': [], 'definitions': []})
-                    if not new_token['detail']:
+                for tokenizer in self.__tokenizers:
+                    token = tokenizer.findToken(line.strip())
+                    if token == tokens.FUNCTION_DECLARATION:
+                        token_name, detailed_name = tokenizer.found_token
+                        new_token = self.__tokens_map.setdefault(
+                            token_name, {'detail': '', 'declarations': [], 'definitions': []})
                         new_token['detail'] = detailed_name
+                        new_token['declarations'].append(file_name+':'+str(number))
+                        self.clearTokenizers()
 
-                    new_token['definitions'].append(file_name+':'+str(number))
-                    self.clearTokenizers()
+                    if token == tokens.FUNCTION_DEFINITION:
+                        token_name, detailed_name = tokenizer.found_token
+                        new_token = self.__tokens_map.setdefault(
+                            token_name, {'detail': '', 'declarations': [], 'definitions': []})
+                        if not new_token['detail']:
+                            new_token['detail'] = detailed_name
 
-                if token == tokens.INCLUDE_DIRECTIVE:
-                    self.clearTokenizers()
+                        new_token['definitions'].append(file_name+':'+str(number))
+                        self.clearTokenizers()
 
-                if token == tokens.MACROS:
-                    self.clearTokenizers()
+                    if token == tokens.INCLUDE_DIRECTIVE:
+                        self.clearTokenizers()
 
-                if token == tokens.ONELINE_COMMENT:
-                    self.clearTokenizers()
+                    if token == tokens.MACROS:
+                        self.clearTokenizers()
 
-                if token == tokens.MULTILINE_COMMENT:
-                    self.clearTokenizers()
+                    if token == tokens.ONELINE_COMMENT:
+                        self.clearTokenizers()
 
-        self.clearTokenizers()
+                    if token == tokens.MULTILINE_COMMENT:
+                        self.clearTokenizers()
+            self.clearTokenizers()
+        except UnicodeDecodeError:
+            print("WARNING: File '" + full_path + "' has encoding different from Unicode and will be skipped")
+
 
     def clearTokenizers(self):
         for tokenizer in self.__tokenizers:
